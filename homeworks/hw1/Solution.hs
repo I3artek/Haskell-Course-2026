18a1c011ee8186324308c49ae92958e0cc061899
Bartlomiej Wzorek <i3artek3@gmail.com>
2026-03-19 21:32:29 +0100

    hw1: task 5
    This was a shitshow, but I managed to write that.
    I know that use of AI was encouraged, but I don't like it.
    So I wrote it myself.
    
    Signed-off-by: Bartlomiej Wzorek <i3artek3@gmail.com>

--- homeworks/hw1/Solution.hs
+++ homeworks/hw1/Solution.hs
@@ -28,3 +28,34 @@ sieve list = go [] list
 
 isPrime :: Int -> Bool
 isPrime n = elem n (primesTo n)
+
+-- Task 5
+
+-- Let's define a nice helper
+applyToEachWithRest :: (a -> [a] -> [[b]]) -> [a] -> [[b]]
+applyToEachWithRest _ [] = []
+applyToEachWithRest f (x : xs) = go [] (x : xs)
+  where
+    go used [] = []
+    go used (x : xs) = (f x (used ++ xs)) ++ (go (used ++ [x]) xs)
+
+insertions :: a -> [a] -> [[a]]
+insertions x [] = [[x]]
+insertions y (x : xs) = go [] (x : xs)
+  where
+    go list [] = [list ++ [y]]
+    go list (x : xs) = [list ++ [y] ++ (x : xs)] ++ (go (list ++ [x]) xs)
+
+getElAndRest :: a -> [a] -> [[([a], [a])]]
+getElAndRest x xs = [[([x], xs)]]
+
+pairmutations :: Int -> [a] -> [([a], [a])]
+pairmutations 0 list = [([], list)]
+pairmutations 1 list = concat (applyToEachWithRest getElAndRest list)
+pairmutations n list = [p | smallerp@(ps, ns) <- smaller, p <- concat (applyToEachWithRest (\x xs -> [[(ps ++ [x], xs)]]) ns)]
+  where
+    smaller = pairmutations (n - 1) list
+
+permutations :: Int -> [a] -> [[a]]
+permutations 0 _ = [[]]
+permutations n list = map (\(x, y) -> x) (pairmutations n list)
