0c33bf37433cd4fe2adedfa06b6371a46445d9f6
Bartlomiej Wzorek <i3artek3@gmail.com>
2026-03-29 09:32:35 +0200

    hw2: another task 5
    Signed-off-by: Bartlomiej Wzorek <i3artek3@gmail.com>

--- homeworks/hw2/Solution.hs
+++ homeworks/hw2/Solution.hs
@@ -1,5 +1,7 @@
 module HW where
 
+import GHC.Base (divInt)
+
 data Sequence a = Empty | Single a | Append (Sequence a) (Sequence a) deriving (Show)
 
 s1 :: Sequence Int
@@ -68,3 +70,20 @@ tailToList ys = go ys []
     go (Single x) (s : tack) = x : go s tack
     go (Append ys zs) stack = go ys (zs : stack)
     go s [] = tailToList s
+
+-- Another task 5
+
+data Token = TNum Int | TAdd | TSub | TMul | TDiv
+
+tailRPN :: [Token] -> Maybe Int
+tailRPN [] = Nothing
+tailRPN list = go list []
+  where
+    go (TAdd : os) (x : y : rest) = go os ((x + y) : rest)
+    go (TSub : os) (x : y : rest) = go os ((x - y) : rest)
+    go (TMul : os) (x : y : rest) = go os ((x * y) : rest)
+    go (TDiv : os) (x : 0 : rest) = Nothing
+    go (TDiv : os) (x : y : rest) = go os ((x `divInt` y) : rest)
+    go ((TNum x) : os) stack = go os (x : stack)
+    go [] [x] = Just x
+    go _ _ = Nothing
